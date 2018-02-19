f107875bcd21e00c80db9e14891ca85057c4b01a
Adelina Tuvenie <atuvenie@cloudbasesolutions.com>
2018-02-19 15:59:07 +0200

    Change some scripts to allow kubetest to run on remote cluster. Some environment variables where overritten.

--- cluster/local/util.sh
+++ cluster/local/util.sh
@@ -19,8 +19,9 @@ function prepare-e2e() {
 }
 
 function detect-master {
-  KUBE_MASTER=localhost
-  KUBE_MASTER_IP=127.0.0.1
-  KUBE_MASTER_URL="http://${KUBE_MASTER_IP}:8080"
-  echo "Using master: $KUBE_MASTER (external IP: $KUBE_MASTER_IP)"
+  #KUBE_MASTER=localhost
+  #KUBE_MASTER_IP=127.0.0.1
+  #KUBE_MASTER_URL="http://${KUBE_MASTER_IP}:8080"
+  #echo "Using master: $KUBE_MASTER (external IP: $KUBE_MASTER_IP)"
+  echo " #### Jumping over this part"
 }
