010edf2bf8cd59086441de01d3d0e68d0e081630
Claudiu Belu <cbelu@cloudbasesolutions.com>
2019-08-20 09:00:56 +0000

    Revert "test images: Bumps image versions"
    This reverts commit 124863cb0af435f11f452ed848d687f1a5dff1d2.

--- test/images/agnhost/agnhost.go
+++ test/images/agnhost/agnhost.go
@@ -48,7 +48,7 @@ import (
 )
 
 func main() {
-	rootCmd := &cobra.Command{Use: "app", Version: "2.11"}
+	rootCmd := &cobra.Command{Use: "app", Version: "2.9"}
 
 	rootCmd.AddCommand(auditproxy.CmdAuditProxy)
 	rootCmd.AddCommand(connect.CmdConnect)
